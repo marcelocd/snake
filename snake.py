6e926721c2239c382e2641ef3e7e8a9ca85aad5c
[marcelocd] <[marcelocdias93@gmail.com]>
2020-07-04 01:47:32 -0300

    Just organizing the code

--- snake.py
+++ snake.py
@@ -2,6 +2,7 @@ import pygame, random
 from pygame.locals import *
 from pygame import mixer
 
+# FUNCTIONS -----------------------------
 def on_grid_random(GRID_LENGTH, GRID_HEIGHT, BASIC_UNIT):
 	x = random.randint(0, GRID_LENGTH - BASIC_UNIT)
 	y = random.randint(0, GRID_HEIGHT - BASIC_UNIT)
@@ -10,6 +11,13 @@ def on_grid_random(GRID_LENGTH, GRID_HEIGHT, BASIC_UNIT):
 def collision(c1, c2):
 	return (c1[0] == c2[0] and c1[1] == c2[1])
 
+# ---------------------------------------
+
+# CONSTANTS -----------------------------
+EASY = 10
+MEDIUM = 20
+HARD = 30
+IMPOSSIBLE = 50
 
 UP = 0
 RIGHT = 1
@@ -32,57 +40,65 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+# ---------------------------------------
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_LENGTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Snake')
 
-score_board = pygame.Surface((SCORE_BOARD_LENGTH, SCORE_BOARD_HEIGHT))
-score_board.fill(BLUE)
-score_board_pos = (GRID_LENGTH + 1, 0)
+# GAME SOUNDS ---------------------------
+eat_sound = mixer.Sound('eat.wav')
+game_over_sound = mixer.Sound('game_over.wav')
 
+# ---------------------------------------
+
+# DEFINING WHERE THE SNAKE CAN CRAWL
 grid = pygame.Surface((GRID_LENGTH, GRID_HEIGHT))
 grid.fill(BLACK)
 grid_pos = (0, 0)
 
+# DEFINING THE SNAKE PROPERTIES
 snake = [(200, 200), (210, 200), (220, 200)]
 snake_skin = pygame.Surface((BASIC_UNIT, BASIC_UNIT))
 snake_skin.fill(WHITE)
+snake_direction = LEFT
 
+#DEFINING THE APPLE PROPERTIES
 apple_pos = on_grid_random(GRID_LENGTH, GRID_HEIGHT, BASIC_UNIT)
 apple = pygame.Surface((BASIC_UNIT, BASIC_UNIT))
 apple.fill(RED)
 
-my_direction = LEFT
+# DEFINING WHERE THE SCORE IS SHOWN
+score_board = pygame.Surface((SCORE_BOARD_LENGTH, SCORE_BOARD_HEIGHT))
+score_board.fill(BLUE)
+score_board_pos = (GRID_LENGTH + 1, 0)
+
+# DEFINING THE SCORE STYLE AND INITIAL VALUE
+score_font = pygame.font.Font('freesansbold.ttf', 25)
+score_pos = (GRID_LENGTH + 10, 10)
+score = 0
 
 clock = pygame.time.Clock()
 
-eat_sound = mixer.Sound('eat.wav')
-game_over_sound = mixer.Sound('game_over.wav')
-
-score_font = pygame.font.Font('freesansbold.ttf', 25)
-score_pos = (GRID_LENGTH + 10, 10)
-
-score = 0
-
 while True:
-	clock.tick(30)
+	clock.tick(EASY)
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 
 		if event.type == KEYDOWN:
 			if event.key == K_UP:
-				if my_direction != DOWN:
-					my_direction = UP
+				if snake_direction != DOWN:
+					snake_direction = UP
 			if event.key == K_RIGHT:
-				if my_direction != LEFT:
-					my_direction = RIGHT
+				if snake_direction != LEFT:
+					snake_direction = RIGHT
 			if event.key == K_DOWN:
-				if my_direction != UP:
-					my_direction = DOWN
+				if snake_direction != UP:
+					snake_direction = DOWN
 			if event.key == K_LEFT:
-				if my_direction != RIGHT:
-					my_direction = LEFT
+				if snake_direction != RIGHT:
+					snake_direction = LEFT
 
 	if collision(snake[0], apple_pos):
 		eat_sound.play()
@@ -99,22 +115,22 @@ while True:
 		snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
 	# (These ifs update the head)
-	if my_direction == UP:
+	if snake_direction == UP:
 		if snake[0][1] == 0:
 			snake[0] = (snake[0][0], GRID_HEIGHT - BASIC_UNIT)
 		else:
 			snake[0] = (snake[0][0], snake[0][1] - BASIC_UNIT)
-	if my_direction == RIGHT:
+	if snake_direction == RIGHT:
 		if snake[0][0] == (GRID_LENGTH - BASIC_UNIT):
 			snake[0] = (0, snake[0][1])
 		else:
 			snake[0] = (snake[0][0] + BASIC_UNIT, snake[0][1])
-	if my_direction == DOWN:
+	if snake_direction == DOWN:
 		if snake[0][1] == (GRID_HEIGHT - BASIC_UNIT):
 			snake[0] = (snake[0][0], 0)
 		else:
 			snake[0] = (snake[0][0], snake[0][1] + BASIC_UNIT)
-	if my_direction == LEFT:
+	if snake_direction == LEFT:
 		if snake[0][0] == 0:
 			snake[0] = (GRID_LENGTH - BASIC_UNIT, snake[0][1])
 		else:
